63249a351bd7738c1b74d9e811b53e5f8914554c
jonas-kgomo <jonaskgmoo@gmail.com>
2021-05-14 12:03:40 +0300

    adding chrome multiple link opening

--- background.js
+++ background.js
@@ -1,19 +1,29 @@
 chrome.runtime.onInstalled.addListener( async () => {
-    const groupTags = document.getElementById('group');
-    chrome.tabGroups.query(query, (tabs) => {
-        groupTags.innerHTML = tabs.map( e => { return `<div>${e.groupId}</div>`;})
-    });
+
+
+
+    // const groupTags = document.getElementById('group');
+    // chrome.tabGroups.query(query, (tabs) => {
+    //     groupTags.innerHTML = tabs.map( e => { return `<div>${e.groupId}</div>`;})
+    // });
+ 
+
+
  });
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
+    const openList = document.getElementById('upload');
     const dialogBox = document.getElementById('demo');
     const dialogList = document.getElementById('list'); 
 //  const data = { totalTabs: dialogBox.innerText, savedTabs: dialogList.innerHTML};
     const saveList = document.getElementById('save');
+    const saveLinks = document.getElementById('open');
     const date = new Date().getDate().toString();
     const month = new Date().getMonth().toString();
     // access to other windows
-  const query = {windowType:'normal'};
+    const query = {windowType:'normal'};
   // gets json fillers
   function saveTabs(text, filename){
         var a = document.createElement('a');
@@ -22,10 +32,47 @@ document.addEventListener('DOMContentLoaded', () => {
         a.click()
     }
 
+//   function getCurrentTab() {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+//     tab = tab[0];
+//     return tab;
+//   });
+// }  
+
+function newTab() {
+ function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+ let actualJSON = JSON.parse(response)
+ loadJSON(function(response) {
+  // Parse JSON string into object
+  let actualJSON = JSON.parse(response);
+ });
+  for(var i=0; i<actual_JSON.length; i++) { 
+  chrome.tabs.create({url : actual_JSON[i].Tabs.url}, function(tab) { 
+      console.log('google script injected'+ tab + url);
+  });
+}
+};
+
+
+
+
+
  //    saveList.addEventListener('click', () => saveTabs(JSON.stringify({Total: tabs.length, Tabs: {...tabs.map(e=> e.url)}}), "data.json"));    
 
 
-     chrome.tabs.query(query, (tabs) => {
+   chrome.tabs.query(query, (tabs) => {
         dialogBox.innerHTML = "Total Tabs : " + tabs.length ;
         dialogList.innerHTML = tabs.map( e => 
           { return `<div class="case" display="flex"> 
@@ -36,12 +83,18 @@ document.addEventListener('DOMContentLoaded', () => {
                   </div>
                   </div>`;
           }).join('');
+        // exporting button   
         saveList.addEventListener('click', () => saveTabs(JSON.stringify(
           {Total: tabs.length, 
            Tabs:  [...tabs.map( e =>  ({url: e.url, title: e.title}))]
           }, null, ' '), "re-" + date + "-" + month +  ".json"));
-  
+        saveLinks.innerHTML = "You currently have " + tabs.length + " URLS  open";
+        // opening a new link
+        openList.addEventListener('click', () => newTab().tab); 
+
  
       });
+
+ 
 });
 
